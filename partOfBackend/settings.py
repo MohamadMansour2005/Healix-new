451ad90e9235011b6e690cdb656aee5175fe63ab
MohamadMansour2005 <mim33@mail.aub.edu>
2025-04-23 08:14:32 -0700

    Update settings.py

--- partOfBackend/settings.py
+++ partOfBackend/settings.py
@@ -64,13 +64,14 @@ WSGI_APPLICATION = 'partOfBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_consultation',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'mohamadmansour',
-        'PASSWORD': 'sqlpass123123!',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
